f939262003696dbc11f766258d57211ecd8177e7
Mischa Taylor <mischa@misheska.com>
2014-10-15 15:10:28 -0700

    Fix for Serverspec v2.20 dependency on json gem - don't remove gcc

--- script/vmtool.sh
+++ script/vmtool.sh
@@ -46,6 +46,3 @@ if [[ $PACKER_BUILDER_TYPE =~ virtualbox ]]; then
         ln -s /opt/VBoxGuestAdditions-4.3.10/lib/VBoxGuestAdditions /usr/lib/VBoxGuestAdditions
     fi
 fi
-
-echo "==> Removing packages needed for building guest tools"
-yum -y remove gcc cpp kernel-devel kernel-headers perl
